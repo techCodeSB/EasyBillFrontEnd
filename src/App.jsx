f16e0f12d77437a7698a3699f9f9e381d92d809c
Anusrimondal <anushreemondaltis@gmail.com>
2025-01-06 12:20:12 +0530

    profile page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const Login = React.lazy(() => import("./pages/Login"));
 const Dashboard = React.lazy(() => import("./pages/Dashboard"));
 const AddQutation = React.lazy(() => import("./pages/quotation/AddQuotation"));
 const Quotation = React.lazy(() => import("./pages/quotation/Quotation"));
+const Profile = React.lazy(() => import("./pages/Profile"));
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="admin/dashboard" element={<Dashboard />} />
         <Route path="/admin/quotation-estimate" element={<Quotation />} />
         <Route path="/admin/quotation-estimate/add" element={<AddQutation/>}/>
+        <Route path="/admin/profile" element={<Profile />} />
       </Routes>
     </Suspense>
 
